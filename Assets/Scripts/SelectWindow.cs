67c5dd1a715440172fee8e9644b35454ffc40a2b
李铁林 <2106643051@qq.com>
2021-11-02 08:45:26 +0800

    first commit

---
+++ Assets/Scripts/SelectWindow.cs
@@ -0,0 +1,58 @@
+﻿/*************************************************
+	作者: Plane
+	邮箱: 1785275942@qq.com
+	日期: 2021/03/06 20:16
+	功能: 英雄选择界面
+
+    //=================*=================\\
+           教学官网：www.qiqiker.com
+           关注微信服务号: qiqikertuts
+           关注微信公众号: PlaneZhong
+               ~~获取更多教学资讯~~
+    \\=================*=================//
+*************************************************/
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectWindow : WindowRoot {
+    public int heroCount;
+    public GameObject heroItem;
+    public Transform scrollRoot;
+    public Button btnSure;
+
+    void Start() {
+        for(int i = 0; i < heroCount; i++) {
+            GameObject go = Instantiate(heroItem);
+            go.transform.SetParent(scrollRoot);
+            go.name = "heroItem_" + i;
+            go.transform.localScale = Vector3.one;
+            OnClickDown(go, ClickItemDown, go, i);
+            OnClickUp(go, ClickItemUp, go, i);
+            OnDrag(go, DragItem, go, i);
+        }
+
+        btnSure.onClick.AddListener(ClickSureBtn);
+    }
+
+    private void ClickItemDown(PointerEventData ped, object[] args) {
+        GameObject go = args[0] as GameObject;
+        int index = (int)args[1];
+        Debug.Log("ClickDown:" + go.name + " index:" + index);
+    }
+    private void ClickItemUp(PointerEventData ped, object[] args) {
+        GameObject go = args[0] as GameObject;
+        int index = (int)args[1];
+        Debug.Log("ClickUp:" + go.name + " index:" + index);
+    }
+    private void DragItem(PointerEventData ped, object[] args) {
+        GameObject go = args[0] as GameObject;
+        int index = (int)args[1];
+        Debug.Log("Drag:" + go.name + " index:" + index);
+    }
+
+    public void ClickSureBtn() {
+        Debug.Log("Click Sure Button.");
+    }
+}
---
+++ Assets/Scripts/WindowRoot.cs
@@ -0,0 +1,57 @@
+﻿/*************************************************
+	作者: Plane
+	邮箱: 1785275942@qq.com
+	日期: 2021/03/07 3:40
+	功能: UI窗口基类
+
+    //=================*=================\\
+           教学官网：www.qiqiker.com
+           关注微信服务号: qiqikertuts
+           关注微信公众号: PlaneZhong
+               ~~获取更多教学资讯~~
+    \\=================*=================//
+*************************************************/
+
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WindowRoot : MonoBehaviour {
+
+    private T GetOrAddComponent<T>(GameObject go) where T : Component {
+        T t = go.GetComponent<T>();
+        if(t == null) {
+            t = go.AddComponent<T>();
+        }
+        return t;
+    }
+
+    protected void OnClick(GameObject go, Action<PointerEventData, object[]> clickCB, params object[] args) {
+        PEListener listener = GetOrAddComponent<PEListener>(go);
+        listener.onClick = clickCB;
+        if(args != null) {
+            listener.args = args;
+        }
+    }
+    protected void OnClickDown(GameObject go, Action<PointerEventData, object[]> clickDownCB, params object[] args) {
+        PEListener listener = GetOrAddComponent<PEListener>(go);
+        listener.onClickDown = clickDownCB;
+        if(args != null) {
+            listener.args = args;
+        }
+    }
+    protected void OnClickUp(GameObject go, Action<PointerEventData, object[]> clickUpCB, params object[] args) {
+        PEListener listener = GetOrAddComponent<PEListener>(go);
+        listener.onClickUp = clickUpCB;
+        if(args != null) {
+            listener.args = args;
+        }
+    }
+    protected void OnDrag(GameObject go, Action<PointerEventData, object[]> dragCB, params object[] args) {
+        PEListener listener = GetOrAddComponent<PEListener>(go);
+        listener.onDrag = dragCB;
+        if(args != null) {
+            listener.args = args;
+        }
+    }
+}
